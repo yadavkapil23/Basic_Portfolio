d5fcf880d4d339d017c99786d07474b6dc0d0a87
Kapil <kydv901@gmail.com>
2024-12-16 22:44:31 +0530

    we added the event section

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 // Smooth scrolling for navigation links
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', event => {
-      event.preventDefault();
-      const targetId = event.target.getAttribute('href').substring(1);
-      const targetSection = document.getElementById(targetId);
-      targetSection.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
+document.querySelectorAll("nav a").forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const targetId = event.target.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+    targetSection.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
     });
   });
-  
-  
-  
+});
